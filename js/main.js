89f1d06cabbc3ae11489e798a9e9085aef71d8ab
BaNtalia <snedgana@mail.ru>
2025-05-05 12:16:17 +0300

    Больше деталей

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const MESSAGES = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+const NAMES = [
+  'Анна',
+  'Борис',
+  'Виктор',
+  'Денис',
+  'Екатерина',
+  'Зинаида',
+  'Ирина,
+  'Кира',
+  'Мария',
+  'Нина',
+];
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const createWizard = () => {
+  const randomNameIndex = getRandomInteger(0, NAMES.length - 1);
+  const randomMessageIndex = getRandomInteger(0, SURNAMES.length - 1);
