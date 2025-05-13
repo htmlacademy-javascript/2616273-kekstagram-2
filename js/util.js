bc6a510e1a920fb674898274809f4ff33039af27
BaNtalia <snedgana@mail.ru>
2025-05-13 14:02:28 +0300

    Модуляция

--- js/util.js
+++ js/util.js
@@ -1,4 +1,10 @@
-import {getRandomInteger, getRandomArrayElement,addPhotos} from'./util.js';
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
 
-export {getRandomInteger, getRandomArrayElement,addPhotos};
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
+export {getRandomArrayElement, getRandomInteger};
